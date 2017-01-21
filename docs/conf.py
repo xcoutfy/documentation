c13a05e8309f66ef1d77139f15b345cfc787d334
Daniele Tartarini <d.tartarini@sheffield.ac.uk>
2017-01-21 15:51:16 +0000

    changed read the doc template

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ plantuml_output_format = 'svg'
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ['.rst','.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
@@ -105,7 +105,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
